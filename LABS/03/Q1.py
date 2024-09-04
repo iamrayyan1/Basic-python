fe40dbdb2e5f0fd1e713314934beded28a0bcc69
Rayyan Merchant <142688529+iamrayyan1@users.noreply.github.com>
2024-09-04 09:58:26 +0500

    Update Q1.py

--- LABS/03/Q1.py
+++ LABS/03/Q1.py
@@ -1,1 +1,20 @@
+# Write a Python program that reads a text file, counts the number of characters in it, and prints the total word count. Handle all possible exceptions as well.
 
+file_name = input("Enter file name: ")
+try:
+    with open(f"{file_name}.txt", 'r') as file:
+
+        content = file.read()
+
+    
+except FileNotFoundError:
+    print(f"The file '{file_name}' was not found.")
+except IOError:
+    print("An error occurred while reading the file.")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+
+else:
+    print(content)
+    print("Char count: ", len(content))
+    print("Word count: ", len(content.split()))
