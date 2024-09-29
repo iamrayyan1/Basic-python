769b98e877d9b26909e056ca36aafeeab477b6bf
Rayyan Merchant <142688529+iamrayyan1@users.noreply.github.com>
2024-09-29 07:29:03 +0200

    Update Q1.py

--- LABS/05/Q1.py
+++ LABS/05/Q1.py
@@ -1,3 +1,8 @@
+# 1. Create a Bus child class that inherits from the Vehicle class. The default fare charge of any
+# vehicle is seating capacity * 100. If Vehicle is Bus instance, we need to add an extra 10% on
+# full fare as a maintenance charge. So total fare for bus instance will become the final amount =
+# total fare + 10% of the total fare. [2 marks]
+
 class Vehicle:
     def __init__(self, seating_cap):
         self.seating_cap = seating_cap
