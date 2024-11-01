5c2869f7cc1ec8be34153dbf9df99146290f7f3a
Rayyan Merchant <142688529+iamrayyan1@users.noreply.github.com>
2024-11-01 19:26:11 +0100

    Create q1.py

---
+++ Assignments/numpy/q1.py
@@ -0,0 +1,8 @@
+# 1. Create a one-dimensional NumPy array containing integers from 1 to 10. Compute the sum, mean, and product of the array.
+
+import numpy as np
+arr = np.arange(1,11)
+print(arr)
+print(np.sum(arr))
+print(np.mean(arr))
+print(np.prod(arr))
